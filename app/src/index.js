46f9df41876b693f1ff5e0389866baea6d819bb7
Anand Chowdhary <mail@anandchowdhary.com>
2017-10-06 19:10:02 +0200

    Add babel

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 function component() {
 	var element = document.createElement('div');
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');	
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	return element;
 }
 
