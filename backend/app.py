672371589303ec8324eb3e0c0cb9e64f12cf264d
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-06 23:24:07 +0200

    adding crud basic

--- backend/app.py
+++ backend/app.py
@@ -18,8 +18,28 @@ from paralleldots import sentiment
 translator = Translator()
 set_api_key(API_KEY)
 
+import pymongo
+from pymongo import Connection
 
-# def
+conn = Connection()
+db = conn['myDB']
+collection = db['language']
+
+#Creating a collection
+db.language.insert({"id": "1", "name": "C", "grade":"Boring"})
+db.language.insert({"id": "2", "name":"Python", "grade":"Interesting"})
+
+#Reading it
+print "After create\n",list(db.language.find())
+
+#Updating the collection
+db.language.update({"name":"C"}, {"$set":{"grade":"Make it interesting"}})
+print "After update\n",list(db.language.find())
+
+#Deleting the collection
+db.language.drop()
+print "After delete\n", list(db.language.find())
+
 
 @app.route('/translate', methods=['GET'])
 def translate_api_call():
