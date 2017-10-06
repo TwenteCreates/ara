a8670dd2461d5eecde252b543e26eb54a4a00224
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-06 19:33:25 +0200

    adding changes for sentiment

--- backend/app.py
+++ backend/app.py
@@ -2,18 +2,22 @@
 
 from flask import Flask
 import boto3
-from googletrans import Translator
-translator = Translator()
 import os,sys
 import json
 import timeit
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from codes import API_KEY
 
 app = Flask(__name__)
 CORS(app)
+from googletrans import Translator
+from paralleldots import set_api_key
+from paralleldots import sentiment
 
+translator = Translator()
+set_api_key(API_KEY)
 
 @app.route('/translate', methods=['GET'])
 def translate_api_call():
@@ -21,6 +25,14 @@ def translate_api_call():
     x = translator.translate('hoe gaat het?')
     resp['value'] = x.text
     return jsonify(resp)
+
+@app.route('/sentiment', methods=['GET'])
+def sentiment_api_call():
+    resp = dict()
+    x = sentiment("It is going to be difficult")
+    resp = x
+    return jsonify(resp)
+
 
 @app.route('/')
 def hello_world():
