09a4c5af6e5b7f23ffbf583f55b152d650c577d6
Anand Chowdhary <mail@anandchowdhary.com>
2017-10-06 19:50:01 +0200

    CSS

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,6 @@
 import _ from "lodash";
+import css from "./assets/sass/basic.scss";
+
 function component() {
 	var element = document.createElement("div");
 	element.innerHTML = _.join(["Hello", "webpack"], " ");
