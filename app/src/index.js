6cfd116e3ded5a6cee599a8d40ea8c5059ad0143
Anand Chowdhary <mail@anandchowdhary.com>
2017-10-06 19:31:45 +0200

    Doing

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,7 @@
-import _ from 'lodash';
-
+import _ from "lodash";
 function component() {
-	var element = document.createElement('div');
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	var element = document.createElement("div");
+	element.innerHTML = _.join(["Hello", "webpack"], " ");
 	return element;
 }
 
