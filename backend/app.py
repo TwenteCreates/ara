7ae5656191699d41e93a435d1173e618e90606b8
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-07 03:25:29 +0200

    fix 500 on last message api

--- backend/app.py
+++ backend/app.py
@@ -70,6 +70,8 @@ def get_last_k_messages():
     res = db.chats1.find({"to":to_user}).sort("_id", -1)
     # lis = []
     # from_set = set()
+    if not res
+        return jsonify({})
     for element in res:
         element.pop('_id')
         print element
