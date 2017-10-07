bb9e941101c7246248ebcff7e402fb7cc36a5235
Anand Chowdhary <mail@anandchowdhary.com>
2017-10-07 12:49:09 +0200

    Add cod

---
+++ app/node_modules/siriwavejs/README.md
@@ -0,0 +1,96 @@
+![image](https://s3.amazonaws.com/f.cl.ly/items/2r2i3p2G262M271N0F2M/siri.gif)
+
+[![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/CaffeinaLab/SiriWaveJS?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+Have you ever thought on how to get the Siri wave effect on your website or mobile app?
+
+SiriWaveJS is a library that *easily* allows  you to get this effect.
+
+**Embed the script... and Surf!**
+
+#### [LIVE EXAMPLE!](http://caffeinalab.github.io/SiriWaveJS)
+
+## Usage
+
+```html
+<div id="siri-container"></div>
+<script src="/path/to/your/siriwave.js"></script>
+<script>
+var siriWave = new SiriWave({
+	container: document.getElementById('siri-container'),
+	width: 640,
+	height: 200,
+	/*
+	speed: 0.2,
+	color: '#000',
+	frequency: 2
+	*/
+});
+</script>
+```
+
+## Constructor options
+
+#### `style` (String, default: 'none')
+
+* `default` - Default iOS9- style
+* `ios9` - Style of for iOS9+
+
+#### `container` (DOM Object)
+
+The DOM container where the `canvas` to draw the wave is added.
+
+#### `[speed]` (Number, from `0` to `1`, default: `0.1`)
+
+The speed of the wave.
+
+#### `[amplitude]` (Number, from `0` to `1`, default: `1`)
+
+The noise (amplitude) of the wave.
+
+#### `[frequency]` (Number, from `0` to `N`, default: `1`)
+
+The frequency of the wave.
+
+**Not available in iOS9 Style**
+
+#### `[color]` (String, Color, default: `#fff`)
+
+The color of the wave, in hexadecimal form (`#336699`, `#FF0`)
+
+**Not available in iOS9 Style**
+
+#### `[cover]` (Boolean, default: `false`)
+
+The `canvas` covers the entire width or height of the container.
+
+#### `[speedInterpolationSpeed]` (Number)
+
+The speed to interpolate the `speed` property.
+
+#### `[amplitudeInterpolationSpeed]` (Number)
+
+The speed to interpolate the `amplitude` property.
+
+## API
+
+#### `start()`
+Start the animation
+
+#### `stop()`
+Stop the animation.
+
+#### `setSpeed(newValue)`
+Set the new value of speed (animated)
+
+#### `setAmplitude(value)`
+Set the new value of amplitude (animated)
+
+## Projects/examples that uses this script
+
+* [The Capitol - The Official Government of Panem (Hunger Games Mockingjay Part 1 website)](http://www.thecapitol.pn/)
+* [http://blog.kidliaa.com/demo/siri%20wave/](http://blog.kidliaa.com/demo/siri%20wave/ )
+
+## Some math
+
+![image](https://cloud.githubusercontent.com/assets/839700/3263201/224d98ec-f26f-11e3-971c-1e87f66a212f.JPG)
--- app/src/index.js
+++ app/src/index.js
@@ -2,23 +2,34 @@ import _ from "lodash";
 import "bootstrap/dist/css/bootstrap.min.css";
 import css from "./assets/sass/basic.scss";
 require("angular");
+
+var moment = require("moment");
+
 let app = angular.module("ara", [require("angular-route")]);
 var chrono = require("chrono-node");
 
 app.config(function($routeProvider) {
 	$routeProvider.when("/:user1/:user2/:lang", {
-		templateUrl : "./views/messages.html?v=" + Math.floor(Math.random() * 1000),
+		templateUrl: "./views/messages.html?v=" + Math.floor(Math.random() * 1000),
 		controller: "messages"
 	}).when("/:user1/:user2", {
-		templateUrl : "./views/messages.html?v=" + Math.floor(Math.random() * 1000),
+		templateUrl: "./views/messages.html?v=" + Math.floor(Math.random() * 1000),
 		controller: "messages"
 	});
 });
 
-app.controller("messages", function($scope, $interval, $http, $routeParams) {
+app.controller("messages", function($scope, $interval, $http, $routeParams, $timeout) {
+	$scope.messages = [];
 	$scope.user1 = $routeParams["user1"];
 	$scope.user2 = $routeParams["user2"];
 	$scope.lang = $routeParams["lang"];
+	$scope.last = null;
+	$scope.speaker = function(text) {
+		if ($scope.last != text) {
+			responsiveVoice.speak(text);
+		}
+		$scope.last = text;
+	}
 	$http.get("http://198.211.125.182:5000/chat?user1=" + $scope.user1 + "&user2=" + $scope.user2).then(function(response) {
 		$scope.messages = response.data;
 		sessionStorage.translatedMessages = JSON.stringify([]);
@@ -38,8 +49,8 @@ app.controller("messages", function($scope, $interval, $http, $routeParams) {
 				if (response.data.length != currLength) {
 					$scope.messages = response.data;
 					currLength = $scope.messages.length;
-					if (sessionStorage.speaking == 1 && $scope.messages[$scope.messages.length - 1].from == "ara") {
-						responsiveVoice.speak($scope.messages[$scope.messages.length - 1].text);
+					if ($scope.messages[$scope.messages.length - 1].from == "ara") {
+						$scope.newMessage($scope.messages[$scope.messages.length - 1]);
 					}
 				}
 			});
@@ -64,7 +75,7 @@ app.controller("messages", function($scope, $interval, $http, $routeParams) {
 								text: text,
 								datetime: chrono.parseDate("right now")
 							});
-							responsiveVoice.speak(text);
+							$scope.newMessage(response.data);
 						} else {
 							console.log("no last message");
 						}
@@ -73,65 +84,176 @@ app.controller("messages", function($scope, $interval, $http, $routeParams) {
 			}
 		}, 5000);
 	});
-	sessionStorage.speaking = 0;
-	$scope.sendMessage = function() {
-		$scope.messages.push({
-			from: $scope.user1,
-			to: $scope.user2,
-			text: $scope.messageModel,
-			datetime: chrono.parseDate("right now")
-		});
-		var a = 0;
-		var handsFree = ["handsfree", "driving", "drive"];
-		for (var i = 0; i < handsFree.length; i++) {
-			if ($scope.messageModel.toLowerCase().indexOf(handsFree[i]) != -1) {
-				a++;
+	$scope.conversation = null;
+	$scope.responster = null;
+
+	$scope.followingup = "";
+	$scope.boolReply = null;
+	$scope.context = null;
+	
+
+	$scope.newMessage = function(message) {
+		console.log(message);
+		$scope.speaker(message.text);
+		if (message.text.indexOf("wants to have a meeting with you") > -1) {
+			$scope.boolReply = 1;
+			$scope.context = message;
+		}
+	};
+
+
+	$scope.reply = function(message, type) {
+		var messageObject = {
+			from: "ara",
+			to: $scope["user1"],
+			datetime: moment().format("MMMM Do YYYY, h:mm:ss a"),
+			text: message
+		};
+		$scope.messages.push(messageObject);
+		$scope.speaker(message);
+		$http.post("http://198.211.125.182:5000/chat", messageObject);
+	}
+
+
+	$scope.sendMessage = function(message) {
+
+		console.log($scope.followingup);
+
+		if (message == null) message = $scope.messageModel;
+
+		var messageObject = {
+			from: $scope["user1"],
+			to: $scope["user2"],
+			datetime: moment().format("MMMM Do YYYY, h:mm:ss a"),
+			text: message
+		};
+		
+		$http.post("http://198.211.125.182:5000/chat", messageObject);
+		$scope.messages.push(messageObject);
+
+		if ($scope.followingup != "") {
+			var messageContent = $scope.followingup + " " + message;
+		} else {
+			var messageContent = message;
+		}
+
+		$http.post("http://198.211.125.182:5000/parser", {
+			query: messageContent
+		}).then(function(response) {
+
+			console.log(response.data);
+
+			if ($scope.boolReply == 1) {
+				if (response.data.intent == "affirmation") {
+					$scope.boolReply = 0;
+					var options = ["Alright, thanks!", "Okay, have a great meeting!", "Got it.", "Alright, sounds good!", "Sounds great, thanks!"];
+					$scope.reply(options[Math.floor(Math.random() * options.length)]);
+					console.log("YES");
+					console.log($scope.context);
+					var message = "Hi " + capitalizeFirstLetter($scope.context.organizer) + ", " + capitalizeFirstLetter($scope.user1) + " has confirmed your meeting " + moment($scope.context.datetime).calendar().toLocaleLowerCase();
+					$http.post("http://198.211.125.182:5000/chat", {
+						from: "ara",
+						to: $scope.context.organizer,
+						text: message,
+						datetime: moment().format("MMMM Do YYYY, h:mm:ss a")
+					});
+					// $http.post("http://198.211.125.182:5000/chat", {
+					// 	from: "ara",
+					// 	to: a.toLowerCase().split(" ")[0],
+					// 	text: "Hi " + capitalizeFirstLetter(a.toLowerCase().split(" ")[0]) + ", " + capitalizeFirstLetter($scope.user1) + " wants to have a meeting with you " + moment(b).calendar().toLowerCase() + ". You're free at " + moment(b).format("LT") + ". Should I finalize it?",
+					// 	datetime: chrono.parseDate("right now")
+					// }).then(function(response) {
+					// 	$http.post("http://198.211.125.182:5000/chat", {
+					// 		from: "ara",
+					// 		to: $scope.user1,
+					// 		text: message,
+					// 		datetime: chrono.parseDate("right now")
+					// 	}).then(function(response) {
+					// 		$scope.speaker(message);
+					// 		$scope.messages.push({
+					// 			from: "ara",
+					// 			to: $scope.user1,
+					// 			text: message,
+					// 			datetime: chrono.parseDate("right now")
+					// 		});
+					// 	});
+					// });
+				} else {
+					var options = ["Aww, that's a bummer. What time do you prefer?"];
+					$scope.reply(options[Math.floor(Math.random() * options.length)]);
+				}
+			
+			} else if (response.data.meeting) {
+				var p = 0;
+				if (response.data.person == "") {
+					$scope.reply("Who are you meeting?");
+					$scope.followingup += " with " + message;
+					p++;
+				} else if (response.data.date == "") {
+					$scope.reply("When is the meeting?");
+					$scope.followingup += " at " + message;
+					p++;
+				} else if (response.data.time == "") {
+					$scope.reply("What time is the meeting?");
+					$scope.followingup += " " + message;
+					p++;
+				}
+				console.log(p);
+				if (p == 0) {
+					$scope.followingup = "";
+					var options = ["Alright, I'm on it!", "Okay, I'm on it!", "Got it.", "Alright, sounds good!", "Sounds great, I'm on it."];
+					$scope.reply(options[Math.floor(Math.random() * options.length)]);
+					var a = response.data.person;
+					var b = response.data.date + " " + response.data.time;
+					$timeout(function() {
+						var message = "I've sent a message to " + a + " about your meeting " + moment(b).calendar().toLocaleLowerCase();
+						$http.post("http://198.211.125.182:5000/chat", {
+							from: "ara",
+							organizer: $scope.user1,
+							to: a.toLowerCase().split(" ")[0],
+							text: "Hi " + capitalizeFirstLetter(a.toLowerCase().split(" ")[0]) + ", " + capitalizeFirstLetter($scope.user1) + " wants to have a meeting with you " + moment(b).calendar().toLowerCase() + ". You're free at " + moment(b).format("LT") + ". Should I finalize it?",
+							datetime: chrono.parseDate("right now")
+						}).then(function(response) {
+							$http.post("http://198.211.125.182:5000/chat", {
+								from: "ara",
+								to: $scope.user1,
+								text: message,
+								datetime: chrono.parseDate("right now")
+							}).then(function(response) {
+								$scope.speaker(message);
+								$scope.messages.push({
+									from: "ara",
+									to: $scope.user1,
+									text: message,
+									datetime: chrono.parseDate("right now")
+								});
+							});
+						});
+					}, 2000);
+				}
+
+			} else if (response.data.driving) {
+				var options = ["Alright, have a safe drive! I'll keep you posted in case anything comes up.", "Alright, have a safe drive! I'll read out your messages now.", "Alright, I'll read out your messages now. Have a safe drive!", "Alright, I'll keep you posted in case anything comes up. Have a safe drive!"];
+				$scope.reply(options[Math.floor(Math.random() * options.length)]);
+			
 			}
-		}
-		if (a == 0) {
-			setTimeout(function() {
-				$scope.typing = 1;
-			}, 100);
-			$http.post("http://198.211.125.182:5000/chat", {
-				from: $scope.user1,
-				to: $scope.user2,
-				text: $scope.messageModel,
-				datetime: chrono.parseDate("right now")
-			}).then(function(response) {
-				console.log("sent");
-			});
-			$scope.messageModel = "";
-		} else if ($scope.user2 == "ara") {
-			responsiveVoice.speak("Okay, I'll read your messages now. Drive safe!");
-			$http.get("http://198.211.125.182:5000/message-list?to=" + $routeParams["user1"]).then(function(response) {
-				$scope.lastMessage = response.data.text;
-				$scope.lastAuthor = response.data.from;
-			});
-			sessionStorage.speaking = 1;
-			$scope.messages.push({
-				from: "ara",
-				to: $scope.user1,
-				text: "Okay, I'll read out your messages. Drive safe!"
-			});
-			$scope.messageModel = "";
-		}
-	}
-	$scope.sendLike = function() {
-		$scope.messageModel = "👍";
-		$scope.sendMessage();
+
+		});
+
 		$scope.messageModel = "";
+
 	}
-	$interval(function() {
-		if (sessionStorage.responseMe) {
-			$scope.messageModel = capitalizeFirstLetter(sessionStorage.responseMe);
-			$scope.sendMessage();
-			$scope.messageModel = "";
-			sessionStorage.removeItem("responseMe");
-		}
-	}, 200);
+
+	$scope.sendLike = function() {
+		$scope.sendMessage("👍");
+	}
+
+	$scope.speaking = 0;
 	$scope.speechtoText = function() {
+		$scope.speaking = 1;
 		listen().then(function(response) {
-			sessionStorage.responseMe = response;
+			$scope.sendMessage(response);
+			$scope.speaking = 0;
 		});
 	}
 	var listen = function() {
@@ -149,6 +271,234 @@ app.controller("messages", function($scope, $interval, $http, $routeParams) {
 			}
 		);
 	}
+
+
+
+	// $scope.meeting = function(a, b) {
+	// 	$timeout(function() {
+	// 		console.log(b);
+	// 		var message = "I've sent a message to " + a + " about your meeting " + moment(b).calendar();
+	// 		$http.post("http://198.211.125.182:5000/chat", {
+	// 			from: "ara",
+	// 			to: a.toLowerCase().split(" ")[0],
+	// 			text: "Hi " + capitalizeFirstLetter(a.toLowerCase().split(" ")[0]) + ", " + capitalizeFirstLetter($scope.user1) + " wants to have a meeting with you " + moment(b).calendar() + ". You're free at " + moment(b).format("LT") + ". Should I finalize it?",
+	// 			datetime: chrono.parseDate("right now")
+	// 		}).then(function(response) {
+	// 			$http.post("http://198.211.125.182:5000/chat", {
+	// 				from: "ara",
+	// 				to: $scope.user1,
+	// 				text: message,
+	// 				datetime: chrono.parseDate("right now")
+	// 			}).then(function(response) {
+	// 				$scope.speaker(message);
+	// 				$scope.messages.push({
+	// 					from: "ara",
+	// 					to: $scope.user1,
+	// 					text: message,
+	// 					datetime: chrono.parseDate("right now")
+	// 				});
+	// 			});
+	// 		});
+	// 	}, 4000);
+	// };
+	// $scope.respond = function(a) {
+	// 	setTimeout(function() {
+	// 		$scope.typing = 1;
+	// 	}, 100);
+	// 	var message = $scope.responster;
+	// 	switch(a) {
+	// 		case "thanks":
+	// 			var messages = ["Happy to help! 😊", "You're welcome! 😊", "Always happy to help! 😊", "No worries! 😊", "Let me know if you need anything else. 😊"];
+	// 			message = messages[Math.floor(Math.random() * messages.length)];
+	// 			break;
+	// 		case "find_time":
+	// 			console.log($scope.responster);
+	// 			if (chrono.parseDate($scope.responster)) {
+	// 				$scope.conversation = null;
+	// 				message = "Sounds great, I'm on it";
+	// 				$scope.meeting($scope.with, chrono.parseDate($scope.responster));
+	// 			} else {
+	// 				message += " When and where is it?";
+	// 				$scope.conversation = "find_time";
+	// 			}
+	// 			break;
+	// 		case "driving_mode":
+	// 			message = "Okay, I'll read your messages now. Drive safe!";
+	// 			sessionStorage.speaking = 1;
+	// 			break;
+	// 		case "meeting_schedule":
+	// 			var withText = message.split("with")[1];
+	// 			withText = withText.split(" ");
+	// 			$scope.with = withText[1];
+	// 			message = "Okay, I'll schedule your meeting with " + withText[1] + ".";
+	// 			if (chrono.parseDate($scope.responster)) {
+	// 				message = "Sounds great! I'm on it.";
+	// 				$scope.meeting($scope.with, chrono.parseDate($scope.responster));
+	// 			} else {
+	// 				message += " When and where is it?";
+	// 				$scope.conversation = "find_time";
+	// 			}
+	// 			break;
+	// 	}
+	// 	$timeout(function() {
+	// 		console.log("responsing to " + a);
+	// 		// var message = "I'm sorry, I don't understand.";
+	// 		$http.post("http://198.211.125.182:5000/chat", {
+	// 			from: "ara",
+	// 			to: $scope.user1,
+	// 			text: message,
+	// 			datetime: chrono.parseDate("right now")
+	// 		}).then(function(response) {
+	// 			$scope.speaker(message);
+	// 			$scope.messages.push({
+	// 				from: "ara",
+	// 				to: $scope.user1,
+	// 				text: message,
+	// 				datetime: chrono.parseDate("right now")
+	// 			});
+	// 			$scope.typing = 0;
+	// 		});
+	// 	}, 1000);
+	// }
+	// $scope.check = function(a, abc) {
+	// 	return new Promise (
+	// 		function (resolve, reject) {
+	// 			if ($scope.conversation) {
+	// 				$scope.responster = abc;
+	// 				$scope.respond($scope.conversation);
+	// 				$http.post("http://198.211.125.182:5000/chat", {
+	// 					from: $scope.user1,
+	// 					to: $scope.user2,
+	// 					text: abc,
+	// 					datetime: chrono.parseDate("right now")
+	// 				}).then(function(response) {
+	// 					resolve({
+	// 						from: $scope.user1,
+	// 						to: $scope.user2,
+	// 						text: abc,
+	// 						datetime: chrono.parseDate("right now")
+	// 					});
+	// 					$scope.messageModel = "";
+	// 				});
+	// 			} else {
+	// 				if (a != "normal_function") {
+	// 					$scope.responster = abc;
+	// 					$scope.respond(a);
+	// 					$http.post("http://198.211.125.182:5000/chat", {
+	// 						from: $scope.user1,
+	// 						to: $scope.user2,
+	// 						text: abc,
+	// 						datetime: chrono.parseDate("right now")
+	// 					}).then(function(response) {
+	// 						resolve({
+	// 							from: $scope.user1,
+	// 							to: $scope.user2,
+	// 							text: abc,
+	// 							datetime: chrono.parseDate("right now")
+	// 						});
+	// 						$scope.messageModel = "";
+	// 					});
+	// 				} else {
+	// 					$http.post("http://198.211.125.182:5000/chat", {
+	// 						from: $scope.user1,
+	// 						to: $scope.user2,
+	// 						text: abc,
+	// 						datetime: chrono.parseDate("right now")
+	// 					}).then(function(response) {
+	// 						resolve({
+	// 							from: $scope.user1,
+	// 							to: $scope.user2,
+	// 							text: abc,
+	// 							datetime: chrono.parseDate("right now")
+	// 						});
+	// 						$scope.messageModel = "";
+	// 					});
+	// 				}
+	// 			}
+	// 		}
+	// 	);
+	// };
+	// sessionStorage.speaking = 0;
+	// $scope.sendMessage = function(abc) {
+	// 	if (abc == null) {
+	// 		abc = $scope.messageModel;
+	// 	}
+	// 	if ($scope.user2 == "ara") {
+	// 		var text = abc.toLowerCase();
+	// 		var messageOptions = [
+	// 			{
+	// 				callback: "driving_mode",
+	// 				words: ["handsfree", "driving", "drive", "earphones"]
+	// 			},
+	// 			{
+	// 				callback: "meeting_schedule",
+	// 				words: ["meeting with", "coffee with", "dinner with", "lunch with"]
+	// 			},
+	// 			{
+	// 				callback: "thanks",
+	// 				words: ["thanks", "thank you"]
+	// 			}
+	// 		];
+	// 		var answer = "normal_function";
+	// 		for (var i = 0; i < messageOptions.length; i++) {
+	// 			var a = 0;
+	// 			for (var j = 0; j < messageOptions[i].words.length; j++) {
+	// 				if (text.indexOf(messageOptions[i].words[j]) > -1) {
+	// 					a++;
+	// 					answer = messageOptions[i].callback;
+	// 				}
+	// 			}
+	// 		}
+	// 		$scope.check(answer, abc).then(function (response) {
+	// 			$scope.messages.push(response);
+	// 		});
+	// 	}
+	// 	// $scope.messages.push({
+	// 	// 	from: $scope.user1,
+	// 	// 	to: $scope.user2,
+	// 	// 	text: $scope.messageModel,
+	// 	// 	datetime: chrono.parseDate("right now")
+	// 	// });
+	// 	// var a = 0;
+	// 	// var handsFree = ["handsfree", "driving", "drive"];
+	// 	// for (var i = 0; i < handsFree.length; i++) {
+	// 	// 	if ($scope.messageModel.toLowerCase().indexOf(handsFree[i]) != -1) {
+	// 	// 		a++;
+	// 	// 	}
+	// 	// }
+	// 	// if (a == 0) {
+	// 	// } else if ($scope.user2 == "ara") {
+	// 	// 	$scope.speaker("Okay, I'll read your messages now. Drive safe!");
+	// 	// 	$http.get("http://198.211.125.182:5000/message-list?to=" + $routeParams["user1"]).then(function(response) {
+	// 	// 		$scope.lastMessage = response.data.text;
+	// 	// 		$scope.lastAuthor = response.data.from;
+	// 	// 	});
+	// 	// 	sessionStorage.speaking = 1;
+	// 	// 	$scope.messages.push({
+	// 	// 		from: "ara",
+	// 	// 		to: $scope.user1,
+	// 	// 		text: "Okay, I'll read out your messages. Drive safe!"
+	// 	// 	});
+	// 	// 	$scope.messageModel = "";
+	// 	// }
+	// 	// console.log($scope.messages);
+	// }
+	// $scope.sendLike = function() {
+	// 	$scope.messageModel = "👍";
+	// 	$scope.sendMessage();
+	// 	$scope.messageModel = "";
+	// }
+	// $interval(function() {
+	// 	if (sessionStorage.responseMe) {
+	// 		$scope.sendMessage(capitalizeFirstLetter(sessionStorage.responseMe));
+	// 		sessionStorage.removeItem("responseMe");
+	// 	}
+	// }, 200);
+	// $scope.speechtoText = function() {
+	// 	listen().then(function(response) {
+	// 		sessionStorage.responseMe = response;
+	// 	});
+	// }
 });
 
 function capitalizeFirstLetter(string) {
